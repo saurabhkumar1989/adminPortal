ec709345781b00addfb73d13382f1749b50e1488
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-05-10 16:33:31 -0400

    ds

--- js/fitness.js
+++
@@ -1,9 +0,0 @@
-var fitness = angular.module('fitness', ['ngTouch'	,'angucomplete-alt','ngResource','nya.bootstrap.select','angularSpinner'])
-	.config(function(nyaBsConfigProvider) {
-      nyaBsConfigProvider.setLocalizedText('en-us', {
-      defaultNoneSelectionTpl: '<span class="label selecttext">Seletc Class</span>',
-      noSearchResultTpl: '<span class="no-match-found">No Matched Result</span>',
-	  selectAll: 'Select All',
-      deselectAll: 'Deselect All'
-    });
-  });
